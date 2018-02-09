bd75a64b62a0b946e3f42a8eb1f8592c6f3e5088
rachitrawat <rachit.rc.rawat@gmail.com>
2018-02-09 16:52:27 +0530

    words, bigrams, trigrams: frequency analysis

---
+++ Assignment2/KeyRecover.py
@@ -0,0 +1,20 @@
+ciphertext_file = "ciphertext.txt"
+ciphertext = ' '.join((([line.rstrip('\n') for line in open(ciphertext_file)])[0]).split())
+
+import pprint
+
+print("\n-----ciphertext-----\n")
+pprint.pprint(ciphertext)
+print()
+
+from collections import Counter
+
+Words_Counter = Counter(x for x in ciphertext if not x.isspace())
+print("\n-----most common words-----\n")
+print(Words_Counter.most_common(5))
+print("\n-----most common bigrams-----\n")
+Bigram_Counter = Counter([ciphertext[i:i + 2] for i in range(len(ciphertext) - 1) if ' ' not in ciphertext[i:i + 2]])
+print(Bigram_Counter.most_common(5))
+print("\n-----most common trigrams-----\n")
+Trigram_Counter = Counter([ciphertext[i:i + 3] for i in range(len(ciphertext) - 1) if ' ' not in ciphertext[i:i + 2]])
+print(Trigram_Counter.most_common(5))
