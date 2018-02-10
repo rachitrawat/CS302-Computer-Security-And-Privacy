304216d3c0555a44743caa56d69b1674a305ff5d
rachitrawat <rachit.rc.rawat@gmail.com>
2018-02-10 19:46:02 +0530

    replace bigrams

--- Assignment2/KeyRecover.py
+++ Assignment2/KeyRecover.py
@@ -1,5 +1,6 @@
 import pprint
 from collections import Counter
+import operator
 
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 k_dict = dict([(alpha[i], alpha[i]) for i in range(len(alpha))])
@@ -17,7 +18,7 @@ letter_freq = {'a': 8.55, 'k': 0.81, 'u': 2.68,
                }
 
 bigrams_common = "TH, HE, IN, ER, AN, RE, ED, ON, ES, ST, EN, AT, TO, NT, HA, ND, OU, EA, NG, AS, OR, TI, IS, ET, IT, AR, TE, SE, HI, OF"
-trigrams_common = "THE, ING, AND, HER, ERE, ENT, THA, NTH, WAS, ETH,FOR, DTH"
+trigrams_common = "THE, ING, AND, HER, ERE, ENT, THA, NTH, WAS, ETH, FOR, DTH"
 bigrams_common_lst = (bigrams_common.replace(' ', '').lower().split(','))
 trigrams_common_lst = (trigrams_common.replace(' ', '').lower().split(','))
 
@@ -30,25 +31,36 @@ def pprintText(header, text):
 def getOverview(ciphertext):
     Letters_Counter = Counter(x for x in ciphertext if not x.isspace())
     print("\n-----most common letters-----")
-    print(Letters_Counter.most_common(5))
+    print(Letters_Counter.most_common(10))
     print("\n-----most common bigrams-----")
     Bigram_Counter = Counter(
         [ciphertext[i:i + 2] for i in range(len(ciphertext) - 1) if ' ' not in ciphertext[i:i + 2]])
-    print(Bigram_Counter.most_common(5))
+    print(Bigram_Counter.most_common(10))
     print("\n-----most common trigrams-----")
     Trigram_Counter = Counter(
         [ciphertext[i:i + 3] for i in range(len(ciphertext) - 1) if ' ' not in ciphertext[i:i + 3]])
-    print(Trigram_Counter.most_common(5))
+    print(Trigram_Counter.most_common(10))
     print()
     return Letters_Counter, Bigram_Counter, Trigram_Counter
 
 
 def replaceChar(cipher_lst, plain_lst, char1, char2):
-    for n, i in enumerate(ciphertext_str):
+    for n, i in enumerate(cipher_lst):
         if i == char1:
             cipher_lst[n] = char2
             plain_lst[n] = char2
     return ''.join(cipher_lst), ''.join(plain_lst)
+
+
+def replaceBigram(cipher_lst, plain_lst, bigram1, bigram2):
+    for n, i in enumerate(cipher_lst):
+        if bigram1 in cipher_lst[n]:
+            tmplst = list(plain_lst[n])
+            tmplst[cipher_lst[n].index(bigram1[0])] = bigram2[0]
+            tmplst[cipher_lst[n].index(bigram1[1])] = bigram2[1]
+            cipher_lst[n] = cipher_lst[n].replace(bigram1, bigram2)
+            plain_lst[n] = "".join(tmplst)
+    return ' '.join(cipher_lst), ' '.join(plain_lst)
 
 
 def getDummyString(str):
@@ -72,6 +84,30 @@ LC, BC, TC = getOverview(ciphertext_str)
 # str with non-space chars replaced with '-'
 plaintext = (getDummyString(ciphertext_str))
 
-# replace most common character with 'e'
-pprintText("'e' replaced text",
-           replaceChar(list(ciphertext_str), list(plaintext), [x[0] for x in LC.most_common(1)][0], 'e'))
+alpha_LF = sorted(letter_freq.items(), key=operator.itemgetter(1), reverse=True)
+
+# Top 5 most common letters in ciphertext
+cipher_LF = [x[0] for x in LC.most_common(5)]
+
+for letter_c in cipher_LF:
+    for letter_p in alpha_LF:
+        # replace 5 most frequent char in cipher with most freq letters in English (till 'r')
+        temp_k_dict = k_dict
+        temp_cipherlst, temp_plainlst = replaceChar(list(ciphertext_str), list(plaintext), letter_c, letter_p[0])
+        pprintText("Swap " + letter_c + " with " + letter_p[0], (temp_cipherlst, temp_plainlst))
+
+        # replace 5 most frequent bigrams in cipher of form -z or z-
+        for bigram_c in BC.most_common(5):
+            if letter_c in bigram_c[0] and bigram_c[1] >= 2:
+                for bigram_p in bigrams_common_lst:
+                    if letter_p[0] in bigram_p:
+                        temp_cipherlst, temp_plainlst = replaceBigram(ciphertext_str.split(), plaintext.split(),
+                                                                      bigram_c[0],
+                                                                      bigram_p)
+
+                        # pprintText("Swap " + bigram_c[0] + " with " + bigram_p, (temp_cipherlst, temp_plainlst))
+                        pprintText("Swap " + bigram_c[0] + " with " + bigram_p,
+                                   replaceChar(list(temp_cipherlst), list(temp_plainlst), letter_c, letter_p[0]))
+
+        if letter_p[0] == 'e':
+            break
