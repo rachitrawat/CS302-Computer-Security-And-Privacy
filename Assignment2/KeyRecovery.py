98c233c946275f0059d03505ab4587985697d84d
rachitrawat <rachit.rc.rawat@gmail.com>
2018-02-11 11:40:02 +0530

    mapping

--- Assignment2/KeyRecovery.py
+++ Assignment2/KeyRecovery.py
@@ -2,7 +2,6 @@ import pprint
 from collections import Counter
 import operator
 
-
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 k_dict = dict([(alpha[i], alpha[i]) for i in range(len(alpha))])
 
@@ -22,6 +21,7 @@ bigrams_common = "TH, HE, IN, ER, AN, RE, ED, ON, ES, ST, EN, AT, TO, NT, HA, ND
 trigrams_common = "THE, ING, AND, HER, ERE, ENT, THA, NTH, WAS, ETH, FOR, DTH"
 bigrams_common_lst = (bigrams_common.replace(' ', '').lower().split(','))
 trigrams_common_lst = (trigrams_common.replace(' ', '').lower().split(','))
+
 
 def pprintText(header, text):
     print("\n-----" + header + "-----")
@@ -43,6 +43,7 @@ def getOverview(ciphertext):
     print()
     return Letters_Counter, Bigram_Counter, Trigram_Counter
 
+
 def getDummyString(str):
     slist = list(str)
     for i, c in enumerate(slist):
@@ -50,10 +51,12 @@ def getDummyString(str):
             slist[i] = '-'
     return ''.join(slist)
 
+
 # Read ciphertext
 ciphertext_file = "ciphertext.txt"
 ciphertext_str = ' '.join((([line.rstrip('\n') for line in open(ciphertext_file)])[0]).split())
 ciphertext_lst = ciphertext_str.split()
+ciphertext_char_lst = list(ciphertext_str)
 
 # pprint
 pprintText("ciphertext", ciphertext_str)
@@ -62,10 +65,12 @@ pprintText("ciphertext", ciphertext_str)
 LC, BC, TC = getOverview(ciphertext_str)
 
 # str with non-space chars replaced with '-'
-plaintext = (getDummyString(ciphertext_str))
+plaintext_str = (getDummyString(ciphertext_str))
+plaintext_lst = plaintext_str.split()
+plaintext_char_lst = list(plaintext_str)
 
 # get single letters in cipher text (=> a or i in English)
-tmplst=[]
+tmplst = []
 for element in ciphertext_lst:
     if len(element) == 1 and element not in tmplst:
         tmplst.append(element)
@@ -73,6 +78,14 @@ for element in ciphertext_lst:
 pprintText("most common single letter words", tmplst)
 
 # Guess (n=>a, v=>i)
-k_dict['a'] = 'n'
-k_dict['i'] = 'v'
+k_dict['n'] = 'a'
+k_dict['v'] = 'i'
 
+# new mapping
+for index, val in enumerate(ciphertext_char_lst):
+    if val != ' ' and val != k_dict[val]:
+        ciphertext_char_lst[index] = k_dict[val]
+        plaintext_char_lst[index] = k_dict[val]
+
+print(''.join(ciphertext_char_lst))
+print(''.join(plaintext_char_lst))
